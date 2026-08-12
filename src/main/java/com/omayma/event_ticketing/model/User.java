b294710b5574b1f6750bf6532fb0a29cd43a2c87
omaymamh <mharziomayma20@gmail.com>
2026-08-12 16:46:38 +0100

    feat: User implements UserDetails + CustomUserDetailsService

--- src/main/java/com/omayma/event_ticketing/model/User.java
+++ src/main/java/com/omayma/event_ticketing/model/User.java
@@ -2,7 +2,11 @@ package com.omayma.event_ticketing.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import java.util.Collection;
+import java.util.List;
 @Entity
 @Table(name="users")
 @Getter
@@ -11,7 +15,7 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy =GenerationType.IDENTITY)
     private Long id;
@@ -25,4 +29,32 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return motDePasse;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
+
 }
